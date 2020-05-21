4c2390df4a032b674a110f6a52fc19baa40247c2
shirk3n <dp.juan@gmail.com>
2020-05-21 12:09:37 +0200

    Refactoring

--- nuts.py
+++ nuts.py
@@ -2,43 +2,38 @@ import pandas as pd
 import json
 
 # geojson files
-PATH_GEOJSON_0 = '/Users/shirk3n/Documents/Programacion/nuts/data/NUTS_LB_2021_4326_LEVL_0.geojson'
-PATH_GEOJSON_1 = '/Users/shirk3n/Documents/Programacion/nuts/data/NUTS_LB_2021_4326_LEVL_1.geojson'
-PATH_GEOJSON_2 = '/Users/shirk3n/Documents/Programacion/nuts/data/NUTS_LB_2021_4326_LEVL_2.geojson'
-PATH_GEOJSON_3 = '/Users/shirk3n/Documents/Programacion/nuts/data/NUTS_LB_2021_4326_LEVL_3.geojson'
+PATH_GEOJSON_0 = 'data/NUTS_LB_2021_4326_LEVL_0.geojson'
+PATH_GEOJSON_1 = 'data/NUTS_LB_2021_4326_LEVL_1.geojson'
+PATH_GEOJSON_2 = 'data/NUTS_LB_2021_4326_LEVL_2.geojson'
+PATH_GEOJSON_3 = 'data/NUTS_LB_2021_4326_LEVL_3.geojson'
 
 # Excel file path
-PATH_EXCEL_FILE = '/Users/shirk3n/Documents/Programacion/nuts/data/Spain1.xlsx'
+PATH_EXCEL_FILE = 'data/Spain1.xlsx'
 
 # Read the xlsx file
 COL_TYPES = {"Country": str, "Name of School": str, "Website": str, "Official Email": str, "NUTs": str}
 SHEET_NAME = 'Sheet1'
 
-def choose_geojson_file(key):
-    length_nut = len(key)
-    #Choose the correct json file depending of the NUT's length
-    if length_nut == 2:
-        print('2')
+def choose_geojson_file(key_excel):
+    lenght_nut = len(key_excel)
+    # Choose the correct json file depending of the NUT's lenght
+    if lenght_nut == 2:
         with open(PATH_GEOJSON_0) as content:
             data = json.load(content)
-    elif length_nut == 3:
-        print('3')
+    elif lenght_nut == 3:
         with open(PATH_GEOJSON_1) as content:
             data = json.load(content)
-    elif length_nut == 4:
-        print('4')
+    elif lenght_nut == 4:
         with open(PATH_GEOJSON_2) as content:
             data = json.load(content)
-    elif length_nut == 5:
-        print('5')
+    elif lenght_nut == 5:
         with open(PATH_GEOJSON_3) as content:
             data = json.load(content)
-    # print(data)
-    print('\n')
     return(data)
 
-def data_geojson(key):
-    data_geojson = choose_geojson_file(key)
+def data_geojson(key_excel):
+    # Create a json data structure
+    data_geojson = choose_geojson_file(key_excel)
     # Data of GEOJSON file
     nuts_names_json = {}
     for features in data_geojson['features']:
@@ -55,7 +50,6 @@ def data_excel(df):
     final_df = sort_row_nut(df)
     for index, row in final_df.iterrows():
         row_nut = row['NUTs']
-        # print(row_nut)
         value = nuts_names_excel.get(row_nut)
         if (value):
             value.append(row)
@@ -64,8 +58,7 @@ def data_excel(df):
     return (nuts_names_excel)
 
 def sort_row_nut(df):
-    row_nut_aux = ''
-    list_aux = []
+    # Replacing the coincident nuts between lenght = 4 and lenght = 5
     for index, row in df.iterrows():
         row_nut = row['NUTs']
         if len(row_nut) == 5:
@@ -75,20 +68,19 @@ def sort_row_nut(df):
     for index1, row1 in df.iterrows():
         row_nut1 = row1['NUTs']
         if row_nut_aux == row_nut1 + '0':
-            print('asdasd')
             df['NUTs'].replace({row1['NUTs']:row_nut_aux},inplace=True)
     return(df)
 
 def iter_excel_item(key, value):
-    # data of each value needed
+    # Data of each value needed
     for key in value:
         for item in value:
             return (item['Country'])
 
 
 def iter_excel(key, value):
+    # Build entryList
     entryList = []
-    # build entryList
     for key in value:
         for item in value:
             entryList.append({'name': item['Name of School'], 'web': item['Website'], 'email': item['Official Email']})
@@ -96,48 +88,42 @@ def iter_excel(key, value):
 
 
 def create_json_kml(df):
-    jsonFinal = {}
+    # Creation of the final json
+    json_final = {}
     list_feature = []
-
+    
     nuts_names_excel = data_excel(df)
+    json_final['type'] = 'FeatureCollection'
 
-    jsonFinal['type'] = 'FeatureCollection'
-
-    for key, value in nuts_names_excel.items():
-        nuts_names_json = data_geojson(key)
-        for jsonId, jsonValue in nuts_names_json.items():
-            if jsonId == key:
-                jsonFinal['features'] = {'type': 'Feature', 'properties':
-                    {"prefix": jsonValue['prefix'],
-                     "country": iter_excel_item(key, value),
-                     "city": jsonValue['city'],
-                     "entryList": iter_excel(key, value)																	},
+    for key_excel, value_excel in nuts_names_excel.items():
+        nuts_names_json = data_geojson(key_excel)
+        for key_json, value_json in nuts_names_json.items():
+            if key_json == key_excel:
+                json_final['features'] = {'type': 'Feature', 'properties':
+                    {"prefix": value_json['prefix'],
+                     "country": iter_excel_item(key_excel, value_excel),
+                     "city": value_json['city'],
+                     "entryList": iter_excel(key_excel, value_excel)																	},
                                          'geometry':{
                                              'type' : 'Point',
-                                             'coordinates': jsonValue['coordinates']
+                                             'coordinates': value_json['coordinates']
                                          }
                                          }
 
-                features = jsonFinal.get('features')
+                features = json_final.get('features')
                 if (features):
                     list_feature.append(features)
 
-    jsonFinal['features'] = list_feature
-    # print(jsonFinal)
+    # Dump a final JSON
+    json_final['features'] = list_feature
+    json_dump = json.dumps(json_final, indent = 2)
 
-    print('\n')
-
-
-
-    json_dump = json.dumps(jsonFinal, indent = 2)
-
-    # writing to a final_result.json
+    # Writing to a final_result.json
     with open ("data/final_result.json","w") as outfile:
         outfile.write(json_dump)
 
 
 if __name__ == '__main__':
-    print('\n')
+
     df = pd.read_excel(PATH_EXCEL_FILE,skiprows=1,sheet_name=SHEET_NAME,dtype=COL_TYPES)
     create_json_kml(df)
-    print(sort_row_nut(df))
